988fc626499604ab55183cb5dd6bb8c1a0ae6308
Alexander Krasyukov <uponef64@gmail.com>
2022-10-25 10:18:47 +0300

    add Callable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,35 @@
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
-       long startTs = System.currentTimeMillis(); // start time
+        long startTs = System.currentTimeMillis(); // start time
 
-        List<Thread> threads = new ArrayList<>();
+        List<FutureTask<Integer>> threads = new ArrayList<>();
 
+        for (String text : texts) {
+            MyCallable callable1 = new MyCallable(text);
+            FutureTask<Integer> integerFutureTask = new FutureTask<>(callable1);
+            new Thread(integerFutureTask).start();
+            threads.add(integerFutureTask);
+        }
+        for (FutureTask<Integer> futureTask : threads) {
             for (String text : texts) {
-                Runnable runnable = () -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            };
-                Thread thread = new Thread(runnable);
-                thread.start();
-                threads.add(thread);
-        }
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+                System.out.println(text.substring(0, 100) + " -> " + futureTask.get());
+            }
         }
 
-      long endTs = System.currentTimeMillis(); // end time
+        long endTs = System.currentTimeMillis(); // end time
 
-     System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Time: " + (endTs - startTs) + "ms");
     }
 
     public static String generateText(String letters, int length) {
